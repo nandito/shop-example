4fa56c2b1c3df764b654d3f197ef0cf28068c832
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-07-05 18:29:08 +0200

    Initial commit, clean up generated code

---
+++ config/project.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+
+export const APOLLO = {
+  uri: 'https://enigmatic-badlands-45171.herokuapp.com/graphql',
+};
+
+export const BUNDLE_ANALYZER = {
+  openAnalyzer: false,
+};
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql, gql } from 'react-apollo';
+
+export default () => (
+  <div>
+  </div>
+);
